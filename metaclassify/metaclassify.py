6fcdb3f4c09648d71d05e64574f8149dc56fd8ca
Yijun Yu <y.yu@open.ac.uk>
2017-11-11 14:23:08 +0000

    extracted features for group of images

--- metaclassify/metaclassify.py
+++ metaclassify/metaclassify.py
@@ -30,22 +30,27 @@ else:
             data.ParseFromString(f.read())
             f.close()
 
-    found = 0
-    for image in data.image:
-        if image.url == sys.argv[1]:
-            found = 1
+    update = 0
+    urls = [line.rstrip('\n') for line in open(sys.argv[1])]
+    for url in urls:
+        print(url)
+        found = 0
+        for image in data.image:
+            if image.url == url:
+                found = 1
 
-    if found == 0:
-        print ("Adding another one")
-        image = data.image.add()
-        image.url = sys.argv[1]
-        prediction = model.predict_by_url(url=image.url)
+        if found == 0:
+            update = 1
+            image = data.image.add()
+            image.url = url
+            prediction = model.predict_by_url(url=image.url)
 
-        for tag in prediction['outputs'][0]['data']['concepts']:
-            feature = image.feature.add()
-            feature.label = tag['name']
-            feature.confidence = tag['value']
+            for tag in prediction['outputs'][0]['data']['concepts']:
+                feature = image.feature.add()
+                feature.label = tag['name']
+                feature.confidence = tag['value']
 
+    if update == 1:
         with open("tags.pb", 'wb') as f:
             serializedMessage = data.SerializeToString()
             f.write(serializedMessage)
