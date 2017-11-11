c6e3a87446e308b22ab61d3b3b5cb4d317cb1fd8
Yijun Yu <y.yu@open.ac.uk>
2017-11-11 15:01:23 +0000

    got new manually examples classified

--- metaclassify/metaclassify.py
+++ metaclassify/metaclassify.py
@@ -29,26 +29,33 @@ else:
         with open("tags.pb", 'rb') as f:
             data.ParseFromString(f.read())
             f.close()
-
+    tags = ""
     update = 0
     urls = [line.rstrip('\n') for line in open(sys.argv[1])]
     for url in urls:
         print(url)
-        found = 0
-        for image in data.image:
-            if image.url == url:
-                found = 1
+        if url.startswith("http"):
+            found = 0
+            for image in data.image:
+                if image.url == url:
+                    found = 1
 
-        if found == 0:
-            update = 1
-            image = data.image.add()
-            image.url = url
-            prediction = model.predict_by_url(url=image.url)
+            if found == 0:
+                update = 1
+                image = data.image.add()
+                image.url = url
+                prediction = model.predict_by_url(url=image.url)
 
-            for tag in prediction['outputs'][0]['data']['concepts']:
-                feature = image.feature.add()
-                feature.label = tag['name']
-                feature.confidence = tag['value']
+                for tag in prediction['outputs'][0]['data']['concepts']:
+                    feature = image.feature.add()
+                    feature.label = tag['name']
+                    feature.confidence = tag['value']
+
+                image.tags = tags
+
+            tags = ""
+        else:
+            tags = tags + " " + url
 
     if update == 1:
         with open("tags.pb", 'wb') as f:
